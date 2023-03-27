fec493731c61e0f57a975fef8862a636af62e632
maxxAbow <mbowman@welland.com>
2023-03-27 19:22:40 -0400

    no duplicate button

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import '../style/home.css';
+import './../style/home.css';
 
-export default function HomeRender({ currentPage, handlePageChange }) {
+export default function HomeRender({currentPage, handlePageChange}) {
     return(
         <div className="container">
-            <button href="#physical" type="button" className={currentPage === 'Physical' ? 'nav-link active' : 'nav-link'} id='homeButton ' onClick={() => handlePageChange('Physical')}>Physical</button>
-            <button href="#virtual" type="button" className={currentPage === 'Virtual' ? 'nav-link active' : 'nav-link'} id='homeButton' onClick={() => handlePageChange('Virtual')}>Virtual</button>
-            <button href="#about" type="button" className={currentPage === 'About' ? 'nav-link active' : 'nav-link'} id='homeButton' onClick={() => handlePageChange('About')}>About</button>
-            <button href="#contact" type="button" className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'} id='homeButton' onClick={() => handlePageChange('Contact')}>Contact</button>
+            <button type="button" className={currentPage === 'Physical' ? 'nav-link active' : 'nav-link'} id='homeButton' onClick={() => handlePageChange('Physical')}>Physical</button>
+            <button type="button" className={currentPage === 'Virtual' ? 'nav-link active' : 'nav-link'} id='homeButton' onClick={() => handlePageChange('Virtual')}>Virtual</button>
+            <button type="button" className={currentPage === 'About' ? 'nav-link active' : 'nav-link'} id='homeButton' onClick={() => handlePageChange('About')}>About</button>
+            <button type="button" className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'} id='homeButton' onClick={() => handlePageChange('Contact')}>Contact</button>
         </div>
     )
 }
--- portfolio/src/homeController.js
+++ portfolio/src/homeController.js
@@ -1,39 +1,31 @@
 import React, {useState} from 'react';
-import './style/home.css';
 import About from './components/about';
 import Physical from './components/physical';
 import Virtual from './components/virtual';
 import Contact from './components/contact';
 import HomeRender from './components/home';
-import HomePage from './components/home';
 
 export default function Home() {
-    const [currentPage, setCurrentPage] = useState('HomePage');
-    const renderPage = () => {
-        console.log('Button has been clicked');
-        if (currentPage === 'HomePage') {
-          return <HomePage />;
-        }
-        if (currentPage === 'About') {
-          return <About />;
-        }
-        if (currentPage === 'Pysical') {
-          return <Physical />;
-        }
-        if (currentPage === 'Virtual') {
-            return <Virtual />;
-          }
-          if (currentPage === 'Contact') {
-            return <Contact />;
-          }
-      };
-
-    const handlePageChange = (page) => setCurrentPage(page);
-
-    return(
-        <div>
+  const [currentPage, setCurrentPage] = useState('Home');
+     const renderPage = () => {
+         if (currentPage === 'Contact') {
+             return <Contact />;
+         }
+         if (currentPage === 'About') {
+           return <About />;
+         }
+         if (currentPage === 'Pysical') {
+           return <Physical />;
+         }
+         if (currentPage === 'Virtual') {
+             return <Virtual />;
+        };
+       };
+  const handlePageChange = (page) => setCurrentPage(page);
+  return (
+    <div>
         <HomeRender currentPage={currentPage} handlePageChange={handlePageChange} />
         {renderPage()}
         </div>
-    );
+  );
 }
